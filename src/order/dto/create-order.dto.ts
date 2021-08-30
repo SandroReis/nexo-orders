c323a45ae63f52a0c206496e3fe4b84c9bea8c76
Sandro Reis <sandrouh@hotmail.com>
2021-08-30 18:52:35 -0300

    refactor: remove unnecessary fields on createOrderDto

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,11 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsAlphanumeric } from 'class-validator';
 
 export class CreateOrderDto {
-  @ApiProperty()
-  @IsAlphanumeric()
-  name: string;
-
   @ApiProperty()
   products: { productId: number; quantity: number }[];
 
